87520c855062db97f49c817558bee62b7f155622
Marshall Brain <marshalldbrain@gmail.com>
2020-09-04 22:19:58 -0400

    Create push pin icon

---
+++ src/components/icons/PushPin.js
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import {createSvgIcon} from "@material-ui/core";
+
+export default createSvgIcon(
+	<path d="M17 4v7l2 3v2h-6v5l-1 1-1-1v-5H5v-2l2-3V4c0-1.1.9-2 2-2h6c1.11 0 2 .89 2 2zM9 4v7.75L7.5 14h9L15 11.75V4H9z" />
+	, 'AcUnit');
